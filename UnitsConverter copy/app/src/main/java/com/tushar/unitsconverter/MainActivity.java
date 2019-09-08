c3a1e02cdbc638f55a0dc521594c8d7b8c831393
tushar sharma <tsharma6@binghamton.edu>
2019-09-07 23:37:14 -0400

    length one way convert

--- UnitsConverter copy/app/src/main/java/com/tushar/unitsconverter/MainActivity.java
+++ UnitsConverter copy/app/src/main/java/com/tushar/unitsconverter/MainActivity.java
@@ -1,13 +1,11 @@
 package com.tushar.unitsconverter;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,35 +16,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button convertButton = findViewById(R.id.button);
+        convertButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                EditText ed1 = findViewById(R.id.editText1);
+                String strInch = ed1.getText().toString();
+                double valInch = Integer.parseInt(strInch);
+
+                double valMtr = valInch * 0.0254;
+                EditText ed2 = findViewById(R.id.editText2);
+                ed2.setText(Double.toString(valMtr));
+
             }
         });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
